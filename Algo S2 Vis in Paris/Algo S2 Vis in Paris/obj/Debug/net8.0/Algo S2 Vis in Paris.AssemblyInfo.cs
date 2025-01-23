5deaa0fa2b62b45fb5f35276751723aae31775ce
alexr2500 <alexandre.rollet@edu.devinci.fr>
2025-01-23 11:13:35 +0100

    Ajout du package Drawing

--- Algo S2 Vis in Paris/Algo S2 Vis in Paris/obj/Debug/net8.0/Algo S2 Vis in Paris.AssemblyInfo.cs
+++ Algo S2 Vis in Paris/Algo S2 Vis in Paris/obj/Debug/net8.0/Algo S2 Vis in Paris.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Algo S2 Vis in Paris")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+580192a18daa9ba8765a084c4c3ba4033f5a3036")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d8e4069345fa2617e558d97cb5068ff4957039cc")]
 [assembly: System.Reflection.AssemblyProductAttribute("Algo S2 Vis in Paris")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Algo S2 Vis in Paris")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
