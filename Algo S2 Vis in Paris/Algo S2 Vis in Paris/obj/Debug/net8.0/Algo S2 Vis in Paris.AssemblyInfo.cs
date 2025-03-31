5acf4ec9f52b166d9feaa8239ac9c55ae53111cd
alexr2500 <alexandre.rollet@edu.devinci.fr>
2025-03-31 18:44:33 +0200

    test

--- Algo S2 Vis in Paris/Algo S2 Vis in Paris/obj/Debug/net8.0/Algo S2 Vis in Paris.AssemblyInfo.cs
+++ Algo S2 Vis in Paris/Algo S2 Vis in Paris/obj/Debug/net8.0/Algo S2 Vis in Paris.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Algo S2 Vis in Paris")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92f20e1880f2febf24a5a17b253d648e310bc0e4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a689102d6503a1fc7527aa64f0c5db6321aef86b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Algo S2 Vis in Paris")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Algo S2 Vis in Paris")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
